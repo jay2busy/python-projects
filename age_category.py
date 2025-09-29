67e65e5f4abe3d4ceab1da395fa5b1480a164321
Jonathan Tembo <tembojonathan241@gmail.com>
2025-09-30 01:14:24 +0200

    Day 10: Python control structures - age catergory assignment

---
+++ age_category.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age =  int(input("Enter your age: "))
+
+if 0 <= age <= 12:
+           category = "Child"
+elif 13 <= age <= 19:
+           category = "A teen"
+elif 20 <= age <= 59:
+           catergory = "An adult"
+elif  age >= 60;
+           catergory = "Senior"
+else:
+           caterogory = "Invalid age"
+
+
